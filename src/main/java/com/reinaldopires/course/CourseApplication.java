2a0acca77aa603707df0febfefe6874bdb3a1f46
reinaldopires94 <reinaldopires94@hotmail.com>
2024-12-20 11:41:42 +0000

    H2 database, test profile, JPA

--- src/main/java/com/reinaldopires/course/CourseApplication.java
+++ src/main/java/com/reinaldopires/course/CourseApplication.java
@@ -8,6 +8,5 @@ public class CourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
-		
 	}
 }
--- src/main/java/com/reinaldopires/course/resources/UserResource.java
+++ src/main/java/com/reinaldopires/course/resources/UserResource.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.reinaldopires.course.entities.User;
 
 @RestController
@@ -13,7 +12,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "João", "joao@hotmail.com", "123456789", "98777");
+		User u = new User(1L, "Romeo", "romoe@hotmail.com", "123456789", "98777");
 		return ResponseEntity.ok().body(u);
 	}
 }
