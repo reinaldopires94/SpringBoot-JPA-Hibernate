8a5416b0a6474a01c0a8d224622b434ae8ab6029
reinaldopires94 <reinaldopires94@hotmail.com>
2025-01-09 13:23:56 +0000

    Add Category class with Database structure/Populate Database with Category objects

--- src/main/java/com/reinaldopires/course/config/TestConfig.java
+++ src/main/java/com/reinaldopires/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.reinaldopires.course.entities.Category;
 import com.reinaldopires.course.entities.Order;
 import com.reinaldopires.course.entities.User;
 import com.reinaldopires.course.entities.enums.OrderStatus;
+import com.reinaldopires.course.repositories.CategoryRepository;
 import com.reinaldopires.course.repositories.OrderRepository;
 import com.reinaldopires.course.repositories.UserRepository;
 
@@ -23,9 +25,19 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1  = new Category(null, "Electronics");
+		Category cat2  = new Category(null, "Books");
+		Category cat3  = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+			
 		User u1 = new User(null, "Maria Brown", "maria@hot.com","01989989899","123456789");
 		User u2 = new User(null, "Mari Campo", "maria@yhaoo.com","02989989899","123456789");
 		User u3 = new User(null, "Mara suko", "maria@gma.com","03989989899","123456789");
